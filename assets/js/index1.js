1f982d55b73ef4d276fe17698072e4be16c42c23
Inha Andriienko <andriienkoinga@gmail.com>
2026-06-11 23:43:14 +0200

    Сделан адаптив второй страницы

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -1,9 +1,16 @@
 const logo = document.querySelector('#logo')
+console.log(logo)
 logo.addEventListener('click', () => {
+    console.log('click')
     window.location.href = './index.html';
 });
 
 const joinBtn = document.querySelector('#btnJoin')
 joinBtn.addEventListener('click', () => {
+    window.location.href = './index2.html';
+});
+
+const signUp = document.querySelector('#signUp')
+signUp.addEventListener('click', () => {
     window.location.href = './index2.html';
 });
